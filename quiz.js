264edd2d6ed40e64c2f758f573ba99013e28690c
Muhammad Adil <adilafsar99@gmail.com>
2021-07-04 06:29:08 +0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -10,7 +10,7 @@ var correctAnswers = 0; // Number of correct answers
 var wrongAnswers = 0; // Number of wrong answers
 var remarks; // Comments on user's score
 var hostName = window.location.toString(); //Current file path
-
+hostName = hostName.slice(0, hostName.lastIndexOf("/") + 1);
 var pageName = hostName.slice(hostName.lastIndexOf("/") + 1); //Current page name
 
 
